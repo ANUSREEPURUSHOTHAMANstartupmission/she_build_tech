90eeb853303cee045eed669ed808ad4b4b9804ac
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-03-10 13:34:14 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+export default defineConfig({
+  integrations: [tailwind(), svelte()],
+  build: {
+    rollupOptions: {
+      external: ['app/js/shortcodes.js'],
+    },
+  },
+});
